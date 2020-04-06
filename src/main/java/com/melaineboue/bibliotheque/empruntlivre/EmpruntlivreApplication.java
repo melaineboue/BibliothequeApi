c6748aba3cbfa3558fe43a5f3d52cfb467b0f588
Boue Melaine <melaine.boue@gmail.com>
2020-04-06 17:15:43 +0200

    First commit

---
+++ src/main/java/com/melaineboue/bibliotheque/empruntlivre/EmpruntlivreApplication.java
@@ -0,0 +1,13 @@
+package com.melaineboue.bibliotheque.empruntlivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpruntlivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpruntlivreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melaineboue/bibliotheque/empruntlivre/EmpruntlivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melaineboue.bibliotheque.empruntlivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpruntlivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
